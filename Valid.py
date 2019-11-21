cd661011fd25878b429b54dabd460467f76e0b55
arjunbista <bistaarjun0@gmail.com>
2019-11-21 09:40:57 +0545

    valid added

---
+++ Valid.py
@@ -0,0 +1,63 @@
+'''import sys
+name=input("Enter your Name: ")
+for x in range(1,4):
+    if name.isalpha()==True and len(name)>=4 or " " in name:
+        pass
+        break
+    else:
+        print("Invalid name")
+        name = input('enter name again')
+else:
+    sys.exit("Please follow next Time")
+age=input("Enter your Age: ")
+for y in range(1,3):
+    if age.isdecimal()==True and int(age)>=18:
+        pass
+        break
+    else:
+        print("Invalid age details")
+        age=input("Re Enter your age: ")
+else:
+    sys.exit("Please follow next Time")
+mob=input("Enter your Mobile number: ")
+for z in range(1,3):
+    if mob.isdigit()==True and len(mob)==10:
+        pass
+        break
+    else:
+        print("INVALID Mobile number")
+        mob=input("Re Enter your mobile: ")
+else:
+    sys.exit("Please follow next Time")
+'''
+#CAlculator code starts here from operation view#
+print("Please choose one of the following operations")
+print("\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Square Root\n6.Square of Number\n7.Cube of Number")
+choice = input("Choose one Option:")
+##user_input == Y##
+while condition==Y:
+    1:\
+        print("You choose Addition Operation")
+    2:print("You choose Subtraction Operation ")
+    3:print("")
+    4:print("")
+    5:print("")
+    6:print("")
+    7:print("")
+    8:print("")
+    else:
+        print("")
+        input=input("Do you want to continue with program again?\nPress Y/n to continue and press N/n to exit")
+        if input==N or input ==n:
+            sys.exit("Program Exiting.....")
+        elif input !=Y or input!=N or input!=y or input!=y:
+            print("Invalid input")
+        else:
+            input =("aaaaa")
+
+
+
+
+
+
+
